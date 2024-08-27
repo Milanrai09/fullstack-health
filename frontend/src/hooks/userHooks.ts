a07a80eeb73f648b9ab9a365271dbdf96a198790
Milan Rai <111414440+Milanrai09@users.noreply.github.com>
2024-08-27 16:14:49 +0530

    Update userHooks.ts

--- frontend/src/hooks/userHooks.ts
+++ frontend/src/hooks/userHooks.ts
@@ -41,7 +41,7 @@ interface GoogleRegisterResponse {
   };
 }
 
-const BASE_URL = import.meta.env.VITE_REACT_APP_BACKEND_BASEURL;
+const BASE_URL = https://fullstack-health-backend.vercel.app;
 
 const api: AxiosInstance = axios.create({ baseURL: BASE_URL });
 
